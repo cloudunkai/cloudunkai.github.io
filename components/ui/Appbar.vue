a14466f5f72700737362b52351587aa0fcd7f2d0
cloudkaiz <zyk2507258@gmail.com>
2023-04-25 20:12:52 +0900

    assets appbar

--- components/ui/Appbar.vue
+++ components/ui/Appbar.vue
@@ -1,9 +1,58 @@
 <template>
   <v-app-bar>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <div v-if="drawer" class="d-flex flex-fill align-center">
+      <v-text-field
+        color="primary"
+        variant="solo"
+        prepend-inner-icon="mdi-magnify"
+        append-inner-icon="mdi-close"
+        hide-details
+        placeholder="Search"
+        @click:append-inner="drawer = false"
+      ></v-text-field>
+    </div>
+    <div v-else class="px-2 d-flex align-center justify-space-between w-100">
+      <!-- ---------------------------------------------- -->
+      <!-- NavIcon -->
+      <!-- ---------------------------------------------- -->
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <div>
+        <v-text-field
+          v-if="mdAndUp"
+          class="ml-2"
+          style="width: 400px"
+          color="primary"
+          variant="solo"
+          density="compact"
+          prepend-inner-icon="mdi-magnify"
+          hide-details
+          placeholder="Search"
+        ></v-text-field>
+      </div>
+      <v-spacer></v-spacer>
+
+      <div>
+        <v-btn v-if="!mdAndUp" icon @click="drawer = true">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <!-- search input desktop -->
+
+        <v-btn v-if="mdAndUp" icon class="text-none">
+          <v-badge dot color="success">
+            <v-icon>mdi-account-multiple-outline</v-icon>
+          </v-badge>
+        </v-btn>
+        <ToolbarLanguage />
+        <ToolbarNotifications />
+        <ToolbarUser />
+      </div>
+    </div>
   </v-app-bar>
 </template>
 <script setup lang="ts">
+import { useDisplay } from 'vuetify'
+const { mdAndUp } = useDisplay()
+
 const drawer = ref(false)
 </script>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,86 +1,9 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-system-bar>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-square</v-icon>
-
-      <v-icon>mdi-circle</v-icon>
-
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar> -->
-
-    <v-navigation-drawer v-model="drawer" absolute color="grey-lighten-3" rail>
-      <v-avatar
-        class="d-block text-center mx-auto mt-4"
-        color="grey-darken-1"
-        size="36"
-      ></v-avatar>
-
-      <v-divider class="mx-3 my-5"></v-divider>
-
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        class="d-block text-center mx-auto mb-9"
-        color="grey-lighten-1"
-        size="28"
-      ></v-avatar>
-    </v-navigation-drawer>
-
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-tabs centered color="grey-darken-2">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-    </v-app-bar>
-
-    <v-main>
-      <NuxtPage />
-      <h3 class="ma-5" style="white-space: break-spaces">{{ data }}</h3>
-    </v-main>
-
-    <!-- <v-navigation-drawer location="right">
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" :title="`Item ${n}`" link>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer> -->
-    <BackToTop />
-    <Snackbar />
-    <v-footer app height="72">
-      <v-text-field
-        v-model="message"
-        bg-color="grey-lighten-1"
-        class="rounded-pill overflow-hidden"
-        density="compact"
-        hide-details
-        variant="solo"
-      ></v-text-field>
-      <v-btn color="" @click="sendMessage"> Sent </v-btn>
-    </v-footer>
+    <UiAppbar />
+    <CommonBackToTop />
+    <CommonSnackbar />
   </v-app>
 </template>
 
-<script setup lang="ts">
-import BackToTop from '@/components/common/BackToTop.vue'
-import Snackbar from '@/components/common/Snackbar.vue'
-
-const loadingStore = useLoadingStore()
-const { chatCompletion } = useChatgpt()
-const drawer = ref(false)
-const links = ref(['Dashboard', 'Messages', 'Profile', 'Updates'])
-
-const data = ref('')
-const message = ref('')
-
-async function sendMessage() {
-  loadingStore.setLoading(true)
-  const response = await chatCompletion(message.value)
-  data.value = response
-  loadingStore.setLoading(false)
-}
-</script>
+<script setup lang="ts"></script>
